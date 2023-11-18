446717de0d83245a326433026581b44195d9b1e7
rafagabidulin <rafagabidulin@gmail.com>
2023-11-18 21:03:21 +0300

    added data

--- src/data/education.ts
+++ src/data/education.ts
@@ -1,3 +1,4 @@
+import { link } from 'fs';
 import IEducation from '../interfaces/IEducation';
 
 const education: IEducation = {
@@ -31,17 +32,20 @@ const education: IEducation = {
           {
             id: 'JavaScriptDomInterfacesBasic',
             name: 'JavaScript/DOM/Интерфейсы для новичков',
-            date: 'Сентябрь 2021 - Ноябрь 2021'
+            date: 'Сентябрь 2021 - Ноябрь 2021',
+            link: 'https://learn.javascript.ru/courses/react-20221018/rafagabidulin/ru/certificate.jpg'
           },
           {
             id: 'JavaScriptDomInterfacesPro',
             name: 'JavaScript/DOM/Интерфейсы для программистов',
-            date: 'Сентябрь 2022 - Октябрь 2022'
+            date: 'Сентябрь 2022 - Октябрь 2022',
+            link: 'https://learn.javascript.ru/courses/js-20220905/rafagabidulin/ru/certificate.jpg'
           },
           {
             id: 'ReactJSCourse',
             name: 'Курс по React',
-            date: 'Октябрь 2022 - Декабрь 2022'
+            date: 'Октябрь 2022 - Декабрь 2022',
+            link: 'https://learn.javascript.ru/courses/react-20221018/rafagabidulin/ru/certificate.jpg'
           }
         ]
       },
@@ -51,7 +55,25 @@ const education: IEducation = {
           {
             id: 'FrontendDeveloper',
             name: 'Профессия «Фронтенд-разработчик»',
-            date: 'Февраль 2022 – Август 2022'
+            date: 'Февраль 2022 – Август 2022',
+            link: 'https://ru.hexlet.io/programs/frontend'
+          }
+        ]
+      },
+      {
+        school: 'ЛАНИТ',
+        courses: [
+          {
+            id: 'ProjectManagement',
+            name: 'Управление проектами: базовый уровень',
+            date: 'Октябрь 2023',
+            link: ''
+          },
+          {
+            id: 'ProjectManagement',
+            name: 'Управление проектами: продвинутый уровень',
+            date: 'Ноябрь 2023',
+            link: ''
           }
         ]
       }
--- src/data/skills.ts
+++ src/data/skills.ts
@@ -5,139 +5,162 @@ const skills: ISkill[] = [
     category: 'Frontend',
     title: 'JavaScript',
     id: 'JavaScript',
-    logo: 'javascript.svg'
+    logo: 'javascript.svg',
+    text: 'Хорошо знаю основы языка'
   },
   {
     category: 'Frontend',
     title: 'TypeScript',
     id: 'TypeScript',
-    logo: 'typescript.svg'
+    logo: 'typescript.svg',
+    text: ''
   },
   {
     category: 'Frontend',
     title: 'React',
     id: 'React',
-    logo: 'react.svg'
+    logo: 'react.svg',
+    text: ''
   },
   {
     category: 'Frontend',
     title: 'Redux',
     id: 'Redux',
-    logo: 'redux.svg'
+    logo: 'redux.svg',
+    text: ''
   },
   {
     category: 'Frontend',
     title: 'Webpack',
     id: 'Webpack',
-    logo: 'webpack.svg'
+    logo: 'webpack.svg',
+    text: 'Использую для сборки проектов'
   },
   {
     category: 'Backend',
     title: 'NodeJS',
     id: 'NodeJS',
-    logo: 'nodejs.svg'
+    logo: 'nodejs.svg',
+    text: ''
   },
   {
     category: 'Testing',
     title: 'Jest',
     id: 'Jest',
-    logo: 'jest.svg'
+    logo: 'jest.svg',
+    text: ''
   },
   {
     category: 'Layout',
     title: 'HTML',
     id: 'HTML',
-    logo: 'html.svg'
+    logo: 'html.svg',
+    text: ''
   },
   {
     category: 'Layout',
     title: 'CSS',
     id: 'CSS',
-    logo: 'css.svg'
+    logo: 'css.svg',
+    text: ''
   },
   {
     category: 'Layout',
     title: 'SASS',
     id: 'SASS',
-    logo: 'sass.svg'
+    logo: 'sass.svg',
+    text: ''
   },
   {
     category: 'Layout',
     title: 'Bootstrap',
     id: 'Bootstrap',
-    logo: 'bootstrap.svg'
+    logo: 'bootstrap.svg',
+    text: ''
   },
   {
     category: 'Other',
     title: 'NPM',
     id: 'NPM',
-    logo: 'npm.svg'
+    logo: 'npm.svg',
+    text: ''
   },
   {
     category: 'Tools',
     title: 'ReactRouter',
     id: 'ReactRouter',
-    logo: 'react-router.svg'
+    logo: 'react-router.svg',
+    text: ''
   },
   {
     category: 'Tools',
     title: 'Babel',
     id: 'Babel',
-    logo: 'babel.svg'
+    logo: 'babel.svg',
+    text: ''
   },
   {
     category: 'Tools',
     title: 'Axios',
     id: 'Axios',
-    logo: 'axios.svg'
+    logo: 'axios.svg',
+    text: ''
   },
   {
     category: 'Tools',
     title: 'ESLint',
     id: 'ESLint',
-    logo: 'eslint.svg'
+    logo: 'eslint.svg',
+    text: ''
   },
   {
     category: 'Tools',
     title: 'Lodash',
     id: 'Lodash',
-    logo: 'lodash.svg'
+    logo: 'lodash.svg',
+    text: ''
   },
   {
     category: 'Tools',
     title: 'Prettier',
     id: 'Prettier',
-    logo: 'prettier.svg'
+    logo: 'prettier.svg',
+    text: ''
   },
   {
     category: 'Tools',
     title: 'Formik',
     id: 'Formik',
-    logo: 'formik.svg'
+    logo: 'formik.svg',
+    text: ''
   },
   {
     category: 'Other',
     title: 'Figma',
     id: 'Figma',
-    logo: 'figma.svg'
+    logo: 'figma.svg',
+    text: ''
   },
   {
     category: 'Other',
     title: 'GitHub',
     id: 'GitHub',
-    logo: 'github.svg'
+    logo: 'github.svg',
+    text: ''
   },
   {
     category: 'Backend',
     title: 'MySQL',
     id: 'MySQL',
-    logo: 'mysql.svg'
+    logo: 'mysql.svg',
+    text: 'Знаю основы системы управления данными. Использовал при разработке данного проекта'
   },
   {
     category: 'Other',
     title: 'VSCode',
     id: 'VSCode',
-    logo: 'vscode.svg'
+    logo: 'vscode.svg',
+    text: 'IDE, которую использую'
   }
 ];
 
